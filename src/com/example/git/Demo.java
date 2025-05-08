b21c219a252d7f6f732331025ae4e2fac98cb124
kru-nal <krunaal.java@gmail.com>
2025-05-08 12:17:14 +0530

    execute this

---
+++ src/com/example/git/Demo.java
@@ -0,0 +1,16 @@
+package com.example.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+	List <Integer>  list = new ArrayList <Integer>();
+	list.add(100);
+	list.add(200);
+	list.add(300);
+		
+	}
+
+}
---
+++ src/com/example/git/HashMapDemo.java
@@ -0,0 +1,19 @@
+package com.example.git;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+	public static void main(String[]args) {
+		
+	Map <String,Integer> map = new HashMap<String,Integer>();
+	
+	map.put("krunal",01);
+	map.put("pratiksha",03);
+	map.put("paresh",02);
+	System.out.println(map);
+	
+	System.out.println("hello form java");
+	
+	}
+
+}
